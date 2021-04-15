ac344f7c5265e17bfbd01fc53fb3449cf7a4c421
NooBitha0 <prakashreddy087@gmail.com>
2021-04-15 18:29:35 +0530

    master_

--- app/src/main/java/com/example/trailproject1/Retailer.java
+++ app/src/main/java/com/example/trailproject1/Retailer.java
@@ -10,5 +10,6 @@ public class Retailer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_retailer);
+        int x = 1;
     }
 }
