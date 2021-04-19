a6ab4fc84c91fc631a1247e80427d1ac669ed87d
NooBitha0 <prakashreddy087@gmail.com>
2021-04-19 15:51:26 +0530

    prakash_8

---
+++ app/src/main/java/com/example/trailproject1/WholesalerOnionSelect.java
@@ -0,0 +1,14 @@
+package com.example.trailproject1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WholesalerOnionSelect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wholesaler_onion_select);
+    }
+}
