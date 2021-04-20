eba27540f4dd54b8942dce732950043b32b19f2f
dharmasai21 <dharmasai0@gmail.com>
2021-04-20 19:28:49 +0530

    sai_2

--- app/src/main/java/com/example/trailproject1/CustomerFruitAppleAddToCartPage.java
+++ app/src/main/java/com/example/trailproject1/CustomerFruitAppleAddToCartPage.java
@@ -27,7 +27,7 @@ public class CustomerFruitAppleAddToCartPage extends AppCompatActivity implement
     String item = "Apple";
     int mod = 3;
     int dup=7;
-    int dup2=7;
+    int dup2=8;
 
     ArrayList<String> vendors;
     ArrayList<RelativeLayout> layoutarr = new ArrayList<>();
