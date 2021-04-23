f437ffa100ed25e5d1d45740c03bd3c71cfac28e
NooBitha0 <prakashreddy087@gmail.com>
2021-04-23 16:22:57 +0530

    prakash_13

---
+++ app/src/main/java/com/example/trailproject1/CustomerPaymentOptions.java
@@ -0,0 +1,14 @@
+package com.example.trailproject1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerPaymentOptions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_payment_options);
+    }
+}
